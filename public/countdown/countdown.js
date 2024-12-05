cf0f5179ee4b777d154e60588895ecb9a7a987f0
Huto <92351427+Hutotpn@users.noreply.github.com>
2024-12-05 10:18:45 +0000

    Tiny update

--- public/countdown/countdown.js
+++ public/countdown/countdown.js
@@ -1,37 +1,37 @@
-// Background images
-const backgrounds = [
-  "https://files.hutotpn.webcr.top/test/pexels-brett-sayles-1708601.jpg",
-  "https://files.hutotpn.webcr.top/test/pexels-daniel-reche-718241-1556654.jpg",
-  "https://files.hutotpn.webcr.top/test/pexels-jeswin-1628419.jpg",
-  "https://files.hutotpn.webcr.top/test/pexels-eftodii-aurelia-90976-735423.jpg",
-];
-
 let bgIndex = 0;
-
-// Preload images
 const preloadedImages = [];
-backgrounds.forEach((url, index) => {
-  const img = new Image();
-  img.onload = () =>
-    console.log(`Image ${index + 1} loaded successfully: ${url}`);
-  img.onerror = () => console.error(`Error loading image ${index + 1}: ${url}`);
-  img.src = url;
-  preloadedImages[index] = img;
-});
 
-function changeBackground() {
-  if (preloadedImages[bgIndex]?.src) {
-    // Apply the preloaded image source directly without wrapping in `url()`
-    document.body.style.backgroundImage = `url(${preloadedImages[bgIndex].src})`;
-  } else {
-    console.error("Background image not loaded yet or missing.");
-  }
-  bgIndex = (bgIndex + 1) % preloadedImages.length; // Cycle through the images
-}
+fetch("./data.json")
+  .then((response) => response.json())
+  .then((data) => {
+    // Preload images
+    data.forEach((url, index) => {
+      const img = new Image();
+      img.onload = () =>
+        console.log(`Image ${index + 1} loaded successfully: ${url}`);
+      img.onerror = () =>
+        console.error(`Error loading image ${index + 1}: ${url}`);
+      img.src = url;
+      preloadedImages[index] = img;
+    });
 
-// Update the background every 10 seconds
-setInterval(changeBackground, 10000);
-changeBackground(); // Initial background set
+    function changeBackground() {
+      if (preloadedImages[bgIndex]?.src) {
+        // Apply the preloaded image source directly without wrapping in `url()`
+        document.body.style.backgroundImage = `url(${preloadedImages[bgIndex].src})`;
+      } else {
+        console.error("Background image not loaded yet or missing.");
+      }
+      bgIndex = (bgIndex + 1) % preloadedImages.length; // Cycle through the images
+    }
+
+    // Update the background every 10 seconds
+    setInterval(changeBackground, 10000);
+    changeBackground(); // Initial background set
+  })
+  .catch((error) => {
+    console.error("Error fetching data:", error);
+  });
 
 // Countdown Logic
 function updateCountdown() {
